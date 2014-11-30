d9a2c02a05402a9412f7e69e9eb87be4dddb7f84
Sergey Nozdrenkov <nozdrenkov@gmail.com>
2014-12-01 01:32:40 +0300

    Lab1

---
+++ Controllers/AlphaController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NSVTmp.Controllers
+{
+    public class AlphaController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Lotos()
+        {
+            return View();
+        }
+
+        public ActionResult Photos()
+        {
+            return View();
+        }
+
+        public ActionResult Bikes()
+        {
+            return View();
+        }
+    }
+}
